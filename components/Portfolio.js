521c1779175c580e14bc0cd7b18f9d54cc60b068
Mehul Oswal <mehuloswal21@gmail.com>
2022-01-27 21:16:05 +0530

    Adding ThirdWebSDK

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { coins } from "../static/coins";
 import Coin from "./Coin";
 import BalanceChart from "./BalanceChart";
+import { ThirdwebSDK } from "@3rdweb/sdk";
+import { ethers } from "ethers";
+
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(
+    process.env.NEXT_PRIVATE_METAMASK_KEY,
+    ethers.getDefaultProvider(
+      "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+    )
+  )
+);
+
 const Portfolio = () => {
+  const [sanityTokens, setSanityTokens] = useState([]);
+  const [thirdWebTokens, setThirdWebTokens] = useState([]);
+  useEffect(() => {
+    const getSanityAndThirdWebTokens = async () => {
+      const coins = await fetch(
+        "https://tlo96hsy.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D'coins'%5D%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+      );
+      const sanityTokens = (await coins.json()).result;
+      setSanityTokens(sanityTokens);
+
+      sanityTokens.map((token) =>
+        console.log(sdk.getTokenModule(token.contractAddress))
+      );
+    };
+    return getSanityAndThirdWebTokens();
+  }, []);
   return (
     <Wrapper>
       <Content>
